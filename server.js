54248e2e10d3cf39838c34646b903640a70470d5
pug-lee <lhc76@yahoo.com>
2025-05-25 16:27:08 +0000

    first test

--- server.js
+++ server.js
@@ -13,8 +13,37 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// child process for python
+const { spawn } = require('child_process');
+
+// Needed for EJS
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Needed for public directory
+app.use(express.static(__dirname + '/public'));
+
+// Needed for parsing form data
+app.use(express.json());       
+app.use(express.urlencoded({extended: true}));
+
+
+/*
 app.get('/', (req, res) => {
 	res.send('Hello remote world!\n');
+}); 
+*/
+
+// main landing page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+
+//about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
 app.listen(PORT, HOST);
