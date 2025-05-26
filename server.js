cec281db8b4ea1687100e88b57abc9092bb6a3fb
pug-lee <lhc76@yahoo.com>
2025-05-26 14:51:32 +0000

    Claude

--- server.js
+++ server.js
@@ -29,12 +29,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-/*
-app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
-}); 
-*/
-
 // main landing page
 app.get('/', function(req, res) {
     res.render('pages/index');
@@ -45,6 +39,38 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
+
+//about page
+app.get('/instructions', function(req, res) {
+    res.render('pages/instructions');
+});
+
+//about page
+app.get('/login', function(req, res) {
+    res.render('pages/login');
+});
+
+//about page
+app.get('/profile', function(req, res) {
+    res.render('pages/profile');
+});
+
+//about page
+app.get('/results', function(req, res) {
+    res.render('pages/results');
+});
+
+//about page
+app.get('/roadmap', function(req, res) {
+    res.render('pages/roadmap');
+});
+
+//about page
+app.get('/signup', function(req, res) {
+    res.render('pages/signup');
+});
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
